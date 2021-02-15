eb358efa5980d812440262fe127b26663a9646ca
Denis Karev <dkarev@protonmail.com>
2021-02-15 07:09:49 +0100

    blacklist ip check

--- models/ip.go
+++ models/ip.go
@@ -54,6 +54,54 @@ func GetIPs(userID string) ([]IP, error) {
 	return result, nil
 }
 
+// Gets IP addresses of the blacklisted users
+func GetBlacklistedIPs() ([]IP, error) {
+	var result []IP
+	rows, err := db.Query(
+		`SELECT web_ips.ip
+			   FROM blacklist
+    			    JOIN web_users ON blacklist.discord_id = web_users.id
+    			    JOIN web_ips   ON web_users.id = web_ips.user_id;`)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return result, nil
+		}
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		nextIP := IP{}
+		err = rows.Scan(&nextIP.UserID, &nextIP.IP, &nextIP.LastUsed)
+		if err != nil {
+			return nil, err
+		}
+		result = append(result, nextIP)
+	}
+	err = rows.Err()
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+// Checks if the specified IP is blacklisted
+func IsBlacklisted(ip string) (bool, error) {
+	ips, err := GetBlacklistedIPs()
+	if err != nil {
+		return false, err
+	}
+
+	for i := 0; i < len(ips); i++ {
+		if ip == ips[i].IP {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
+
 func (ip *IP) SetLastUsed(lastUsed time.Time) error {
 	_, err := db.Exec("UPDATE web_ips SET last_used = ? WHERE ip = ? AND user_id = ?",
 		lastUsed.Format("2006-01-02 15:04:05"), ip.IP, ip.UserID)
